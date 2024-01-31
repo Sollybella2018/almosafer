b445a40c18a9786e28c1bb5b8f738b4f232d7eb8
Your NameSollybella2018 <assilomar@hotmail.com>
2024-01-31 19:20:06 +0300

    update

---
+++ src/almosaferproject/mynewtest.java
@@ -0,0 +1,19 @@
+package almosaferproject;
+
+import org.testng.annotations.BeforeTest;
+
+public class mytest extends parameters {
+	
+	
+	@BeforeTest
+	
+	public void MySetUp () {
+	
+	driver.get(URL);
+	driver.win
+	
+	}
+	
+	
+	
+}
---
+++ src/almosaferproject/parameters.java
@@ -0,0 +1,19 @@
+package almosaferproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class parameters {
+ 
+	
+	WebDriver driver=new ChromeDriver();
+	
+	String URL = "https://global.almosafer.com/en"; 
+	
+	String [] Websites= {"https://global.almosafer.com/en","https://global.almosafer.com/ar"};
+
+
+	
+}
